7454be54e138c985bb0d0e926526beb7d649255c
iconoservices <iconoservices@example.com>
2026-06-21 19:10:31 -0500

    PWA: change to registerType prompt so update banner shows

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'prompt',
       includeAssets: ['favicon.svg', 'favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'AlDía - Tu Cerebro Digital',
